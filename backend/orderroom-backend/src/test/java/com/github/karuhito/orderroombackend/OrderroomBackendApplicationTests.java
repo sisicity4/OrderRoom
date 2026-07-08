48157d9868a76be127a6bbb9c67752d24ea49e3f
Karuhito <179291384+Karuhito@users.noreply.github.com>
2026-07-08 13:24:01 +0900

    add: springbootの環境構築に伴うファイルを追加

---
+++ backend/orderroom-backend/src/test/java/com/github/karuhito/orderroombackend/OrderroomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.karuhito.orderroombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderroomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
