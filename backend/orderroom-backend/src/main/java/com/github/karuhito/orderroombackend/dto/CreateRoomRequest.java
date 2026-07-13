76b2bfb757b7961bc4062128747c5405388a2fdc
Karuhito <179291384+Karuhito@users.noreply.github.com>
2026-07-13 22:53:30 +0900

    バリデーション失敗時のメッセージを追記

--- backend/orderroom-backend/src/main/java/com/github/karuhito/orderroombackend/dto/CreateRoomRequest.java
+++ backend/orderroom-backend/src/main/java/com/github/karuhito/orderroombackend/dto/CreateRoomRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CreateRoomRequest(
-    @NotBlank
+    @NotBlank(message = "空白は許可されていません")
     @Size(max = 100)
     String title,
 
