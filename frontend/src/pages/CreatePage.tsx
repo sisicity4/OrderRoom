48225a8635011e266e103cbb795e8c71b4a1f932
Nari1011 <213721812+Nari1011@users.noreply.github.com>
2026-07-13 15:20:13 +0900

    ルーム作成画面の入力フォームを実装

--- frontend/src/pages/CreatePage.tsx
+++ frontend/src/pages/CreatePage.tsx
@@ -1,4 +1,39 @@
-function CreatePage(){
-  return <h1>ルーム作成画面</h1>
+import { useState } from 'react'
+
+function CreatePage() {
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState('');
+  const [memo, setMemo] = useState('');
+
+  return (
+    <>
+      <h1>ルーム作成画面</h1>
+
+      <input
+        type="text"
+        placeholder="タイトル"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input 
+      type="date"
+      placeholder='日付'
+      value={date}
+      onChange={(e) =>
+        setDate(e.target.value)
+      } />
+
+      <textarea
+      value={memo}
+      placeholder='メモ'
+      onChange={(e) =>
+        setMemo(e.target.value)
+      } />
+
+      <button onClick={() => console.log({title,date,memo})}>作成</button>
+      
+      
+    </>
+  );
 }
 export default CreatePage;
