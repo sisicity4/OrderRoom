c92485f27463b58747cf72f2f683ca9e584bb5c4
Karuhito <179291384+Karuhito@users.noreply.github.com>
2026-07-14 16:32:52 +0900

    参加者追加のrequest部分を記述

--- backend/orderroom-backend/src/main/java/com/github/karuhito/orderroombackend/dto/CreateParticipantRequest.java
+++ backend/orderroom-backend/src/main/java/com/github/karuhito/orderroombackend/dto/CreateParticipantRequest.java
@@ -1,13 +1,9 @@
 package com.github.karuhito.orderroombackend.dto;
 
-import com.github.karuhito.orderroombackend.entity.Room;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CreateParticipantRequest(
-    Room room,
-
     @NotBlank(message = "名前を正しく入力してください")
     @Size(max = 50, message = "名前は50字以内で入力してください")
     String name
