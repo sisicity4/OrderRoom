a4c5296b0847e0653f67d151df46b8d0e7b73ffc
Karuhito <179291384+Karuhito@users.noreply.github.com>
2026-07-14 16:32:05 +0900

    参加者追加のresponse部分を記述

---
+++ backend/orderroom-backend/src/main/java/com/github/karuhito/orderroombackend/dto/CreateParticipantResponse.java
@@ -0,0 +1,14 @@
+package com.github.karuhito.orderroombackend.dto;
+
+import java.time.Instant;
+import java.util.UUID;
+
+
+public record CreateParticipantResponse(
+    UUID id,
+    UUID roomId, //getRoom().getId()
+    String name,
+    UUID token,
+    Instant createdAt
+) {
+}
