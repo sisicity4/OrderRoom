45fe5f9e82fbf588793fcca1293ee666c074e426
Karuhito <179291384+Karuhito@users.noreply.github.com>
2026-07-17 16:55:18 +0900

    ItemListのResponse部分を記述

---
+++ backend/orderroom-backend/src/main/java/com/github/karuhito/orderroombackend/dto/ItemListResponse.java
@@ -0,0 +1,22 @@
+package com.github.karuhito.orderroombackend.dto;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import com.github.karuhito.orderroombackend.entity.ItemStatus;
+
+public record ItemListResponse(
+    UUID id,
+    UUID roomId,
+    UUID participantId,
+    String participantName,
+    String name,
+    int price,
+    int quantity,
+    String memo,
+    ItemStatus status,
+    boolean purchased,
+    Instant createdAt,
+    Instant UpdatedAt
+) {
+}
