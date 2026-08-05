db9d7dfc2381bd97ba9047b0ca218e66078f802a
Karuhito <179291384+Karuhito@users.noreply.github.com>
2026-08-05 12:55:03 +0900

    コンストラクタとgetterを追加しvalueで取得する形に変更

--- backend/orderroom-backend/src/main/java/com/github/karuhito/orderroombackend/entity/ItemStatus.java
+++ backend/orderroom-backend/src/main/java/com/github/karuhito/orderroombackend/entity/ItemStatus.java
@@ -1,5 +1,20 @@
 package com.github.karuhito.orderroombackend.entity;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum ItemStatus {
-    PROPOSED,ACCEPTED,REJECTED
+    PROPOSED("proposed"),
+    ACCEPTED("accepted"),
+    REJECTED("rejected");
+
+    private final String value;
+
+    ItemStatus(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
 }
